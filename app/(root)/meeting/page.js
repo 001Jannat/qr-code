3d7a2d3228dd3a2d9ee4c5296f41878f0df44132
Jannat Khatoon <jkhatoon276@gmail.com>
2024-12-13 22:16:30 +0530

    fix

--- app/(root)/meeting/page.js
+++ app/(root)/meeting/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react';
-import QRCodePage from '../qrCode/qrCode';
+import QRCodePage from '../qrCode/page';
 
 
 export default function MeetingPage() {
